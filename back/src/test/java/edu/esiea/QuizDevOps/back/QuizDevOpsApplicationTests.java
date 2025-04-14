4ca8ac166fbb440c05f132334debbc8018dec1d3
ABDELKRIM LIMÉRY Zion <blackzev01@gmail.com>
2025-04-14 09:37:59 +0200

    Init Project

---
+++ back/src/test/java/edu/esiea/QuizDevOps/back/QuizDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.esiea.QuizDevOps.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizDevOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
