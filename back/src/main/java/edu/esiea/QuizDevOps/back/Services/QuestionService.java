4fe9803b7fb70df5e6a55af19889a65ade785502
ABDELKRIM LIMÉRY Zion <blackzev01@gmail.com>
2025-04-14 14:01:38 +0200

    Développement des services

---
+++ back/src/main/java/edu/esiea/QuizDevOps/back/Services/QuestionService.java
@@ -0,0 +1,27 @@
+package edu.esiea.QuizDevOps.back.Services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.esiea.QuizDevOps.back.Entities.QuestionEntity;
+import edu.esiea.QuizDevOps.back.Repositories.QuestionRepository;
+
+@Service
+public class QuestionService {
+    @Autowired
+    private QuestionRepository repo;
+
+	public QuestionService(QuestionRepository repo) {
+		this.repo = repo ;
+	}
+	
+    public QuestionEntity createQuestion(QuestionEntity question) {
+        return repo.save(question);
+    }
+
+    public List<QuestionEntity> readByQuizId(Long quizId) {
+        return repo.findByQuizId(quizId);
+    }
+}
