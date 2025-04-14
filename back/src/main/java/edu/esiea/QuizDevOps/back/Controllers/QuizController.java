9f4ce470265fbf9ba976a35d5b83d09e7a4fe104
ABDELKRIM LIMÉRY Zion <blackzev01@gmail.com>
2025-04-14 23:44:42 +0200

    Fix : rename readAll(), change name of mapping for @Put and @Delete

--- back/src/main/java/edu/esiea/QuizDevOps/back/Controllers/QuizController.java
+++ back/src/main/java/edu/esiea/QuizDevOps/back/Controllers/QuizController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +29,7 @@ public class QuizController {
     }
 
     @GetMapping("/all")
-    public List<QuizEntity> getAll() {
+    public List<QuizEntity> readAll() {
         return service.readAll();
     }
     
@@ -36,14 +38,15 @@ public class QuizController {
         return ResponseEntity.ok(service.readById(id));
     }
     
-    @PostMapping("/update/{id}")
+    @PutMapping("/update/{id}")
     public ResponseEntity<QuizEntity> update(@PathVariable Long id, @RequestBody QuizEntity quiz) {
     	QuizEntity updatedQuiz = service.update(id, quiz);
         return ResponseEntity.ok(updatedQuiz);
     }
     
-    @PostMapping("/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+    	service.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 }
