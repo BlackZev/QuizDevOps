f6f9dd2401e1c56f3eea1b028b14236db6f3a288
ABDELKRIM LIMÉRY Zion <blackzev01@gmail.com>
2025-04-14 23:47:37 +0200

    Fix : add @Entity annotation

--- back/src/main/java/edu/esiea/QuizDevOps/back/Entities/UserAnswerEntity.java
+++ back/src/main/java/edu/esiea/QuizDevOps/back/Entities/UserAnswerEntity.java
@@ -1,10 +1,12 @@
 package edu.esiea.QuizDevOps.back.Entities;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 
+@Entity
 public class UserAnswerEntity {
 	// Fields
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
